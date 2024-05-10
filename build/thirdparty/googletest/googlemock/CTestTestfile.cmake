369c2a58a250933dafe9da029549287ce379dd7b
LLWWZW <LLWWZW@github.com>
2024-05-10 10:01:08 +0200

    updating the methods of planner

---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lwzhou/cpp_projects/Build_an_OpenStreetMap_Route_Planner/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
+# Build directory: /home/lwzhou/cpp_projects/Build_an_OpenStreetMap_Route_Planner/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lwzhou/cpp_projects/Build_an_OpenStreetMap_Route_Planner/CppND-Route-Planning-Project/thirdparty/googletest/googletest
+# Build directory: /home/lwzhou/cpp_projects/Build_an_OpenStreetMap_Route_Planner/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
